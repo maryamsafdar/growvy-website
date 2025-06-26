99bb1a6c312f9499dd1dc4ffc658d59335e3ef9b
maryamsafdar <maryamsafdar453@gmail.com>
2025-06-26 15:02:38 +0500

    first commit

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,7 +13,7 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
-      <body>
+      <body className='bg-white'>
         <Navbar />
         {children}
         <Footer />
